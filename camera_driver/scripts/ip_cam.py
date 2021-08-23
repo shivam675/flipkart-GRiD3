57df2cd24df7bb7230a60bbe446f0e4878d362e4
shivam675 <shivam31199@gmail.com>
2021-08-23 14:39:47 +0530

    Done

--- camera_driver/scripts/ip_cam.py
+++ camera_driver/scripts/ip_cam.py
@@ -12,28 +12,26 @@ from cv_bridge import CvBridge, CvBridgeError
 #url = 'http://localhost:4747/mjpegfeed'
 # url  = 'http://56.161.165.117:8080/video'
 # url = 'http://192.168.43.147:4747/video'
-url = "http://192.168.179.241:8080/video"
 
-cap = cv2.VideoCapture(url)
 bridge = CvBridge()
 info = CameraInfo()
 
 
 # info.
 
-def video_cap():
-    ip_pub_compressed = rospy.Publisher("/ip_cam/image_compressed", CompressedImage, queue_size=10)
-    ip_pub_raw = rospy.Publisher("/ip_cam/image_raw", Image, queue_size=10)
+def video_cap(w,h,id):
+    ip_pub_compressed = rospy.Publisher("/usb_cam/image_raw/compressed", CompressedImage, queue_size=10)
+    ip_pub_raw = rospy.Publisher("/usb_cam/image_raw", Image, queue_size=10)
     # ip_pub_caminfo = rospy.Publisher("/ip_cam/camera_info", CameraInfo, queue_size=10)
     rospy.loginfo("INIT DONE !")
     while not rospy.is_shutdown():
         rospy.loginfo_once("IMAGE PUBLISING RN")
         # width=1920
         # height=1080
-        width=1280
-        height=720
+        # width=1280
+        # height=720
         ret, frame = cap.read()
-        dim = (width, height)
+        dim = (w, h)
         img = cv2.resize(frame, dim)
         # img = cv2.rotate(img, cv2.ROTATE_180)
         if img is not None:
@@ -41,6 +39,8 @@ def video_cap():
             # rospy.loginfo("first if cleared")
             img_msg_comp = bridge.cv2_to_compressed_imgmsg(img)
             img_msg_raw = bridge.cv2_to_imgmsg(img, encoding="bgr8")
+            img_msg_raw.header.frame_id = id
+            img_msg_comp.header.frame_id = id
             ip_pub_compressed.publish(img_msg_comp)
             ip_pub_raw.publish(img_msg_raw)
     cv2.destroyAllWindows()
@@ -49,6 +49,16 @@ def video_cap():
 
 if __name__ == "__main__":
     rospy.init_node("ipcamera_python", anonymous=True)
+    w = rospy.get_param('image_width', default=1280)
+    h = rospy.get_param('image_height', default=720)
+    id = rospy.get_param('camera_frame_id', default='camera_link')
+    ip = rospy.get_param('ip_of_camera', default='192.168.0.35')
+    port = rospy.get_param('port_of_camera', default=8080)
+    
+    url = 'http://' + ip + ':' + str(port) + '/video'
+    cap = cv2.VideoCapture(url)
+
+    
     rospy.sleep(1)
-    video_cap()
+    video_cap(w,h,id)
     # rospy.spin()
